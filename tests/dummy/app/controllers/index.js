1c551326bc39ed6623b50ccdcabefb092dcf1bdd
James Hill <james@onehilltech.com>
2021-03-14 13:09:53 -0400

    chore: updated demo

---
+++ tests/dummy/app/controllers/index.js
@@ -0,0 +1,16 @@
+import Controller from '@ember/controller';
+
+export default class IndexController extends Controller {
+  get props () {
+    return {
+      '--r': {
+        from: '0',
+          to: '1turn'
+      },
+      '--tx': {
+        from: '-100px',
+          to: '100px'
+      }
+    }
+  }
+}
